c99a702009401aaed2b1a7a356a55eb1f15f5e73
tothzalan <toth.zalan03@gmail.com>
2022-09-03 16:33:04 +0200

    mqtt settings

--- data/www/api.php
+++ data/www/api.php
@@ -7,11 +7,18 @@ enum Sources {
     case SYSTEM;
 };
 
+enum RequestType {
+    case GET;
+    case POST;
+}
+
 class Data {
     private Sources $type;
+    public RequestType $reqType;
 
     function __construct() {
         if(isset($_GET['src'])) {
+            $this->reqType = RequestType::GET;
             switch ($_GET['src']) {
                 case 'sensor':
                     $this->type = Sources::SENSOR;
@@ -28,6 +35,8 @@ class Data {
                 default:
                     $this->type = Sources::SYSTEM;
             }
+        } else {
+            $this->reqType = RequestType::POST;
         }
     }
 
@@ -37,34 +46,55 @@ class Data {
                 return $this->sensorData();
             case Sources::SYSTEM:
                 return $this->systemData();
+            case Sources::MQTT:
+                return $this->mqttData();
             /*
             case Sources::WIFI:
-            case Sources::MQTT:
             */
         }
     }
 
+    function post() {
+        return ['body' => 'success'];
+    }
+
     function sensorData() {
         $type = 'all';
-        if(isset($_GET['type'])) {
+        if(isset($_GET['type']))
             $type = $_GET['type'];
-        }
-        if($type == 'all') {
+        if($type == 'all')
             return ['temperature' => rand(-30,50), 'humidity' => rand(0,100)];
-        }
-        else if($type == 'temperature') {
+        else if($type == 'temperature')
             return ['temperature' => rand(-30,50)];
-        }
-        else if($type == 'humidity') {
+        else if($type == 'humidity')
             return ['humidity' => rand(0,100)];
-        }
     }
 
     function systemData() {
-        return ['systemtime' =>  microtime(true)];
+        return ['systemTime' =>  microtime(true)];
+    }
+
+    function mqttData() {
+        $info = 'all';
+        if(isset($_GET['info']))
+            $info = $_GET['info'];
+        if($info == 'all')
+            return ['brokerIP' => '192.168.1.2', 'brokerPort' => 1883,
+                'deviceName' => 'MockAPI', 'username' => 'Gipsz Jakab'];
+        else if($info == 'brokerip')
+            return ['brokerIP' => '192.168.1.2'];
+        else if($info == 'brokerport')
+            return ['brokerPort' => 1883];
+        else if($info == 'devicename')
+            return ['deviceName' => 'MockAPI'];
+        else if($info == 'username')
+            return ['username' => 'Gipsz Jakab'];
     }
 };
 
 header('Access-Control-Allow-Origin: *');
 $data = new Data();
-echo json_encode($data->get());
+if($data->reqType == RequestType::GET)
+    echo json_encode($data->get());
+else
+    echo json_encode($data->post());
